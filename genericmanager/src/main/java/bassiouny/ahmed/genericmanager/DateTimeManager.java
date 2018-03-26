b9049c33ace4546ec0c40fefb6c9456b21b63d63
bassiouny <ahmedbassiouny26@gmail.com>
2018-03-26 22:30:32 +0200

    add timestamp in time manager

--- genericmanager/src/main/java/bassiouny/ahmed/genericmanager/DateTimeManager.java
+++ genericmanager/src/main/java/bassiouny/ahmed/genericmanager/DateTimeManager.java
@@ -11,17 +11,18 @@ import java.util.Locale;
  */
 
 public class DateTimeManager {
+    private static final Locale LOCAL = Locale.ENGLISH;
 
     public static String changeDateFormatWithException(String dateStr, String originalFormat, String targetFormat) throws ParseException {
-        DateFormat original = new SimpleDateFormat(originalFormat, Locale.ENGLISH);
-        DateFormat target = new SimpleDateFormat(targetFormat, Locale.ENGLISH);
+        DateFormat original = new SimpleDateFormat(originalFormat, LOCAL);
+        DateFormat target = new SimpleDateFormat(targetFormat, LOCAL);
         Date date = original.parse(dateStr);
         return target.format(date);
     }
 
     public static String changeDateFormat(String dateStr, String originalFormat, String targetFormat) {
-        DateFormat original = new SimpleDateFormat(originalFormat, Locale.ENGLISH);
-        DateFormat target = new SimpleDateFormat(targetFormat, Locale.ENGLISH);
+        DateFormat original = new SimpleDateFormat(originalFormat, LOCAL);
+        DateFormat target = new SimpleDateFormat(targetFormat, LOCAL);
         try {
             Date date = original.parse(dateStr);
             return target.format(date);
@@ -30,12 +31,11 @@ public class DateTimeManager {
         }
     }
 
-    public static long getTimeStamp() {
+    public static long getCurrentTimeStamp() {
         return System.currentTimeMillis() / 1000;
     }
 
     public static Date convertStringToDate(String date, String strFormat) {
-
         DateFormat format = new SimpleDateFormat(strFormat, Locale.ENGLISH);
         try {
             return format.parse(date);
@@ -45,7 +45,29 @@ public class DateTimeManager {
     }
 
     public static Date convertStringToDateWithException(String date, String strFormat) throws ParseException {
-        DateFormat format = new SimpleDateFormat(strFormat, Locale.ENGLISH);
+        DateFormat format = new SimpleDateFormat(strFormat, LOCAL);
         return format.parse(date);
+    }
+
+    public static long convertStringToUnixTimeStampWithException(String date ,String strFormat) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(strFormat,LOCAL);
+        Date parsedDate = dateFormat.parse(date);
+        return parsedDate.getTime();
+    }
+    public static long convertStringToUnixTimeStamp(String date ,String strFormat) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(strFormat,LOCAL);
+        try {
+            Date parsedDate = dateFormat.parse(date);
+            return parsedDate.getTime();
+        } catch (ParseException e) {
+            return 0;
+        }
+
+    }
+
+    public static String convertUnixTimeStampToString(long unixTimeStamp,String strFormat) {
+        Date date = new Date(unixTimeStamp);
+        DateFormat format = new SimpleDateFormat(strFormat, Locale.ENGLISH);
+        return format.format(date);
     }
 }
